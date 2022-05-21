4a5350a10c4e9eff56b03f8f9d438a38a84c0e6f
Mehmet Fatih Okuyan <okuyanf6161@gmail.com>
2022-05-22 01:58:24 +0300

    fix: shopping cart mechanism

--- main.js
+++ main.js
@@ -27,9 +27,47 @@ function addProduct() {
 }
 
 function addToBasket(product_id) {
-    basket.push(db_products[product_id - 1]);
-    sessionStorage.setItem("basket", JSON.stringify(basket));
+    // TODO: Sepete eklendiğinde animasyon eklenecek
+    // Check if product is already in basket
+    let product = db_products.filter(i => i.product_id == product_id)[0];
+    let product_in_basket = basket.filter(i => i.product_id == product_id);
+    if (product_in_basket.length > 0) {
+        increaseCount(product_id);
+    } else {
+        product.count = 1;
+        basket.push(product);
+        sessionStorage.setItem("basket", JSON.stringify(basket));
+    }
     hideProduct();
+}
+
+function deleteFromBasket(product_id) {
+    let product_index = basket.findIndex(i => i.product_id == product_id);
+    basket.splice(product_index, 1);
+    sessionStorage.setItem("basket", JSON.stringify(basket));
+    updateBasket();
+}
+
+function increaseCount(product_id) {
+    let product = basket.filter(i => i.product_id == product_id)[0];
+    product.count++;
+    let index_of_product = basket.findIndex(product => product.product_id === product_id);
+    basket.splice(index_of_product, 1, product);
+    sessionStorage.setItem("basket", JSON.stringify(basket));
+    updateBasket();
+}
+
+function decreaseCount(product_id) {
+    let product = basket.filter(i => i.product_id == product_id)[0];
+    if (product.count > 1) {
+        product.count--;
+        let index_of_product = basket.findIndex(product => product.product_id === product_id);
+        basket.splice(index_of_product, 1, product);
+    } else {
+        deleteFromBasket(product_id);
+    }
+    sessionStorage.setItem("basket", JSON.stringify(basket));
+    updateBasket();
 }
 
 function showProduct(product_id) {
@@ -74,7 +112,9 @@ function toggleBasket() {
                     <tbody id="basket_item_table_body">
                     <tr>
                         <th>Product</th>
+                        <th style="text-align: right">Count</th>
                         <th style="text-align: right">Price</th>
+                        <th style="text-align: right">Delete</th>
                     </tr>
                     </tbody>
                 </table>`;
@@ -86,7 +126,9 @@ function toggleBasket() {
                 table.innerHTML += `
                 <tr>
                     <td>${product.product_name}</td>
-                    <td style="text-align: right">${numberWithCommas(product.product_price)} ₺</td>
+                    <td style="text-align: right"><button onclick="decreaseCount(${product.product_id})" style="border: none">&#45;</button>${product.count}<button onclick="increaseCount(${product.product_id})" style="border: none">&#43;</button></td>
+                    <td style="text-align: right">${numberWithCommas(product.product_price * product.count)} ₺</td>
+                    <td style="text-align: right"><button onclick="deleteFromBasket(${product.product_id})" style="background: none; border: none">&#10060;</button></td>
                 </tr>
                 `;
             }
@@ -118,10 +160,64 @@ function toggleBasket() {
     }
 }
 
+function updateBasket() {
+    let basketDiv = document.getElementById("basket_items");
+    basketDiv.innerHTML = "";
+    if (basket.length > 0) {
+
+        basketDiv.innerHTML = `
+                <table>
+                    <tbody id="basket_item_table_body">
+                    <tr>
+                        <th>Product</th>
+                        <th style="text-align: right">Count</th>
+                        <th style="text-align: right">Price</th>
+                        <th style="text-align: right">Delete</th>
+                    </tr>
+                    </tbody>
+                </table>`;
+
+        let table = document.getElementById("basket_item_table_body");
+
+        for (let i = 0; i < basket.length; i++) {
+            let product = basket[i];
+            table.innerHTML += `
+                <tr>
+                    <td>${product.product_name}</td>
+                    <td style="text-align: right"><button onclick="decreaseCount(${product.product_id})" style="border: none">&#45;</button>${product.count}<button onclick="increaseCount(${product.product_id})" style="border: none">&#43;</button></td>
+                    <td style="text-align: right">${numberWithCommas(product.product_price * product.count)} ₺</td>
+                    <td style="text-align: right"><button onclick="deleteFromBasket(${product.product_id})" style="background: none; border: none">&#10060;</button></td>
+                </tr>
+                `;
+        }
+
+        basketDiv.innerHTML += `
+                <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+                <div>
+                <h3 style="text-align: right">Total Price: ${numberWithCommas(totalPrice())} ₺</h3>
+                </div>
+                <div style="display: flex">
+                <button class="button white-button" onclick="toggleBasket()">Continue Shopping</button>
+                <div style="padding: 0 10px"></div>
+                <button class="button" onclick="checkout()">Checkout</button>
+                </div>
+            `;
+
+    } else {
+        basketDiv.innerHTML = `
+            <div class="item">
+                <h2 style="text-align: center; color: red">Your Shopping Cart is Empty</h2>
+            </div>
+            <button class="button white-button" id="checkout" onclick="toggleBasket()">Continue Shopping</button>
+            `;
+    }
+
+}
+
 function totalPrice() {
     let total = 0;
     for (let i = 0; i < basket.length; i++) {
-        total += parseInt(basket[i].product_price);
+        total += parseInt(basket[i].product_price * basket[i].count);
     }
     return total;
 }
@@ -160,7 +256,8 @@ function showHome() {
     document.getElementById("shop").style.display = "block";
     document.getElementById("product").style.display = "none";
     document.getElementById("checkout").style.display = "none";
-    document.getElementById("basket").style.display = "none";}
+    document.getElementById("basket").style.display = "none";
+}
 
 function removeClass(element, className) {
     element.classList.remove(className);
